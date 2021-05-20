60bbb0a45a96123677f994ec7b8037f34a74cec0
엘풍 <eruhoon@gmail.com>
2021-05-21 00:26:00 +0900

    setup project

---
+++ app/src/main/java/xyz/mycast/sagitta/MainActivity.kt
@@ -0,0 +1,18 @@
+package xyz.mycast.sagitta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.mycast.sagitta.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/xyz/mycast/sagitta/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package xyz.mycast.sagitta.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
